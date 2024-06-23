2540ecad678506967e556be895a7f1c997b49ae2
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2024-06-23 20:11:13 +0530

    Added validation to Signin and Register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,8 +2,66 @@ import './Register.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 export default function Register() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [passWord, setPassWord] = useState('');
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+  const [userNameError, setUserNameError] = useState('');
+  const [passWordError, setPassWordError] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (firstName.length === 0) {
+      setFirstNameError('Firstname is required!');
+    }
+    if (lastName.length === 0) {
+      setLastNameError('Lastname is required!');
+    }
+    if (userName.length === 0) {
+      setUserNameError('Username is required!');
+    }
+    if (passWord.length === 0) {
+      setPassWordError('Password is required!');
+    } else if (passWord.length < 6) {
+      setPassWordError('Password must be more than 6 characters!');
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+
+    switch (name) {
+      case 'firstName':
+        setFirstNameError('');
+        setFirstName(value);
+        break;
+
+      case 'lastName':
+        setLastNameError('');
+        setLastName(value);
+        break;
+
+      case 'userName':
+        setUserNameError('');
+        setUserName(value);
+        break;
+
+      case 'passWord':
+        setPassWordError('');
+        setPassWord(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="register">
       <div className="registerContainer">
@@ -11,34 +69,46 @@ export default function Register() {
           <FontAwesomeIcon className="registerAvatarImage" icon={faUser} />
         </div>
         <h1>Register</h1>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="registerInputContainer">
-            <div className="registerFirstLastName">
-              <input
-                type="text"
-                placeholder="First Name"
-                id="firstname"
-                name="firstname"
-              />
-              <input
-                type="text"
-                placeholder="Last Name"
-                id="lastname"
-                name="lastname"
-              />
+            <div className="firstLastNameContainer">
+              <div className="firstName">
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  name="firstName"
+                  value={firstName}
+                  onChange={handleChange}
+                />
+                <label>{firstNameError}</label>
+              </div>
+              <div className="lastName">
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  name="lastName"
+                  value={lastName}
+                  onChange={handleChange}
+                />
+                <label>{lastNameError}</label>
+              </div>
             </div>
             <input
               type="text"
-              name="username"
-              id="username"
+              name="userName"
               placeholder="Username"
+              value={userName}
+              onChange={handleChange}
             />
+            <label>{userNameError}</label>
             <input
               type="password"
-              name="password"
-              id="password"
+              name="passWord"
               placeholder="Password"
+              value={passWord}
+              onChange={handleChange}
             />
+            <label>{passWordError}</label>
             <button className="registerButton">Register</button>
           </div>
         </form>
--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -2,8 +2,49 @@ import './Signin.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Signin() {
+  const [username, setUsername] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  // Implement the handleSubmit method
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (username.length === 0) {
+      setUsernameError('Username is required!');
+    }
+
+    if (password.length === 0) {
+      setPasswordError('Password is required!');
+    } else if (password.length <= 6) {
+      setPasswordError('Password must be more than 6 characters!');
+    }
+  };
+
+  // Implement the HandleChange method
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+
+    switch (name) {
+      case 'username':
+        setUsernameError('');
+        setUsername(value);
+        break;
+
+      case 'password':
+        setPasswordError('');
+        setPassword(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="signin">
       <div className="signinContainer">
@@ -11,20 +52,26 @@ export default function Signin() {
           <FontAwesomeIcon className="avatarImage" icon={faUser} />
         </div>
         <h1>Login</h1>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="signinInputContainer">
             <input
               type="text"
               name="username"
               id="username"
               placeholder="Username"
+              value={username}
+              onChange={handleChange}
             />
+            <label className="labelError">{usernameError}</label>
             <input
               type="password"
               name="password"
               id="password"
               placeholder="Password"
+              value={password}
+              onChange={handleChange}
             />
+            <label className="labelError">{passwordError}</label>
             <button className="loginButton">Login</button>
           </div>
         </form>
